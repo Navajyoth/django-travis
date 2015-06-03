a19cae24908510fa705569c0f60107454f757c8e
Navajyoth <navajyothms1989@gmail.com>
2015-06-03 16:10:14 +0530

    fab introduced

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import local
+
+def prepare_deploy():
+    local("./manage.py makemigrations --settings=settings.dev")
+    local("./manage.py migrate --settings=settings.dev")
+    local("./manage.py test apps.account --settings=settings.dev")
+    #local("git add -p && git commit")
+    local("git add .")
+    local("git commit -am 'fab introduced'")
+    local("git push origin master")
+
+
