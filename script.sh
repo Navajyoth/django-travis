040cd172d6534dd0de3d8dffd03a4a71df9557e1
Navajyoth <navajyothms1989@gmail.com>
2016-03-09 11:46:00 +0530

    test commit

---
+++ script.sh
@@ -0,0 +1,3 @@
+#VENV_DIR=/home/navajyoth/.virtualenvs/bamboo/bin
+#echo "Loading virtualenv 'bamboo'... "
+#. $VENV_DIR/activate 
--- settings/production.py
+++ settings/production.py
@@ -4,9 +4,24 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-import dj_database_url
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'abcd1234',
+        'OPTIONS': {
+            # "autocommit": True,
+        },
+    }
+}
+
+
+#import dj_database_url
+#DATABASES = {}
+#DATABASES['default'] =  dj_database_url.config()
 
 BASE_URL = "http://app.com/"
 
