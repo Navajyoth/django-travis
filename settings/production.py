fd1dffd7b32e0c3bed6b8562edafef3724e47562
Navajyoth <navajyothms1989@gmail.com>
2016-03-09 12:00:53 +0530

    logging@rawdatatech.com added

--- settings/production.py
+++ settings/production.py
@@ -26,7 +26,7 @@ DATABASES = {
 BASE_URL = "http://app.com/"
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rdtcontacttree@gmail.com'
-EMAIL_HOST_PASSWORD = 'invincible123#$'
+EMAIL_HOST_USER = 'logging@rawdatatech.com'
+EMAIL_HOST_PASSWORD = 'Abcd123#$'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
