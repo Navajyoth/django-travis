a2017d66956f1ac22e678f581397dddaeeb739c2
Navajyoth <navajyothms1989@gmail.com>
2016-03-09 11:51:35 +0530

    trigger added

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #VENV_DIR=/home/navajyoth/.virtualenvs/bamboo/bin
 #echo "Loading virtualenv 'bamboo'... "
 #. $VENV_DIR/activate 
+cd /home/navajyoth/workspace/django-travis
+pip install -r requirements.txt
