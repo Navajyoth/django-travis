28cc84fb38cdfa0c0a48c8c6f336a63913fba33f
Navajyoth <navajyothms1989@gmail.com>
2016-03-09 12:31:50 +0530

    succesfull ci test

--- script.sh
+++
@@ -1,5 +0,0 @@
-#VENV_DIR=/home/navajyoth/.virtualenvs/bamboo/bin
-#echo "Loading virtualenv 'bamboo'... "
-#. $VENV_DIR/activate 
-cd /home/navajyoth/workspace/django-travis
-pip install -r requirements.txt
